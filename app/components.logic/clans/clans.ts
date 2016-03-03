1b7e74665b508f3bda750ab0c73b59f1038dc167
Dmitry Vasilyev <vaseker@yandex-team.ru>
2016-03-04 03:42:10 +0500

    Enabled async again

--- app/components.logic/clans/clans.ts
+++ app/components.logic/clans/clans.ts
@@ -2,8 +2,8 @@ import { Component, Inject } from 'angular2/core'
 import { RouteConfig, RouterOutlet, ROUTER_DIRECTIVES, AsyncRoute } from 'angular2/router'
 import Store from '../../services/store'
 import I18NPipe from '../../pipes/i18n'
-import { ClansList } from './clans-list'
-import { ClansDetail } from './clans-detail'
+//import { ClansList } from './clans-list'
+//import { ClansDetail } from './clans-detail'
 
 @Component({
     selector: 'clans',
@@ -11,7 +11,6 @@ import { ClansDetail } from './clans-detail'
     pipes: [I18NPipe],
     styles: [require('../../components.common/sub-nav/sub-nav.styl')],
     template: `<nav>
-      <!--<a [routerLink]="['./PlayersSearch']" class="nav-link">{{'clans:search' | i18n}}</a>-->
       <a [routerLink]="['./ClansList']" class="nav-link">{{'clans.list' | i18n}}</a>
       <a *ngFor="#clan of clans" [routerLink]="['./ClansDetail', { abbr: clan }]" class="nav-link">{{clan}}</a>
     </nav>
@@ -20,11 +19,10 @@ import { ClansDetail } from './clans-detail'
 
 
 @RouteConfig([
-    //{ path: '/search', name: 'PlayersSearch', component: PlayersSearchComponent, useAsDefault: true },
-    { path: '/', component: ClansList, name: 'ClansList', useAsDefault: true },
-    { path: '/:abbr', component: ClansDetail, name: 'ClansDetail' }/*
+    /*{ path: '/', component: ClansList, name: 'ClansList', useAsDefault: true },
+    { path: '/:abbr', component: ClansDetail, name: 'ClansDetail' }*/
     new AsyncRoute({ path: '/', loader: () => require('es6-promise!./clans-list')('ClansList'), name: 'ClansList', useAsDefault: true }),
-    new AsyncRoute({ path: '/:abbr', loader: () => require('es6-promise!./clans-detail')('ClansDetail'), name: 'ClansDetail' })*/
+    new AsyncRoute({ path: '/:abbr', loader: () => require('es6-promise!./clans-detail')('ClansDetail'), name: 'ClansDetail' })
 ])
 
 export class Clans {
--- app/components.logic/matches/matches.ts
+++ app/components.logic/matches/matches.ts
@@ -2,9 +2,9 @@ import { Component, Inject } from 'angular2/core'
 import { RouteConfig, RouterOutlet, ROUTER_DIRECTIVES, AsyncRoute } from 'angular2/router'
 import Store from '../../services/store'
 import I18NPipe from '../../pipes/i18n'
-import { MatchesList } from './matches-list'
-import { MatchesListCW } from './matches-list-cw'
-import { MatchesDetail } from './matches-detail'
+//import { MatchesList } from './matches-list'
+//import { MatchesListCW } from './matches-list-cw'
+//import { MatchesDetail } from './matches-detail'
 
 @Component({
     selector: 'matches',
@@ -19,12 +19,12 @@ import { MatchesDetail } from './matches-detail'
 })
 
 @RouteConfig([
-    { path: '/', name: 'MatchesList', component: MatchesList, useAsDefault: true },
+    /*{ path: '/', name: 'MatchesList', component: MatchesList, useAsDefault: true },
     { path: '../clanwars', name: 'MatchesCWList', component: MatchesListCW },
-    { path: '/:match', name: 'MatchesDetail', component: MatchesDetail }/*
+    { path: '/:match', name: 'MatchesDetail', component: MatchesDetail }*/
     new AsyncRoute({ path: '/list', name: 'MatchesList', loader: () => require('es6-promise!./matches-list')('MatchesList'), useAsDefault: true }),
-    new AsyncRoute({ path: '/clanwars', name: 'MatchesCWList', loader: () => require('es6-promise!./matches-list-cw')('MatchesListCW') }),
-    new AsyncRoute({ path: '/:match', name: 'MatchesDetail', loader: () => require('es6-promise!./matches-detail')('MatchesDetail') })*/
+    new AsyncRoute({ path: '../clanwars', name: 'MatchesCWList', loader: () => require('es6-promise!./matches-list-cw')('MatchesListCW') }),
+    new AsyncRoute({ path: '/:match', name: 'MatchesDetail', loader: () => require('es6-promise!./matches-detail')('MatchesDetail') })
 ])
 
 export class Matches {
