c48810c214c20725fdbf0c99444c2a2939b7a11f
Dmitry Vasilyev <vaseker@yandex-team.ru>
2016-06-05 16:53:51 +0500

    Player details number formatting

--- app/components.logic/players/players-detail-counts.ts
+++ app/components.logic/players/players-detail-counts.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core'
 import { I18NPipe } from '../../pipes/i18n'
 import { PercentPipe } from '../../pipes/percent'
+import { NumberPipe } from '../../pipes/number'
 
 @Component({
     selector: 'players-detail-counts',
-    pipes: [I18NPipe, PercentPipe],
+    pipes: [I18NPipe, PercentPipe, NumberPipe],
     inputs: ['data'],
     template: require('./players-detail-counts.html')
 })
