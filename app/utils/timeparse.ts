4c081ed669fe26a2160c29b4d8d9320e0b1fb276
Dmitry Vasilyev <vaseker@yandex-team.ru>
2016-06-19 20:25:55 +0500

    Datetime localized format

--- app/utils/timeparse.ts
+++ app/utils/timeparse.ts
@@ -1,19 +1,37 @@
 import leadZeros from './leadzeros'
+import { LANGUAGE } from '../services/i18n'
 
 export default function timeParse (date, options ?:any) :string {
     if (! (date instanceof Date)) {
         date = new Date(date);
     }
     options = options || {};
+
+    if (LANGUAGE.lang === 'en') {
+        if (options.slim) {
+            return leadZeros(date.getMonth() + 1) + '/' +
+                leadZeros(date.getDate()) + ' ' +
+                leadZeros(date.getHours()) + ':' +
+                leadZeros(date.getMinutes());
+        }
+
+        return date.getFullYear() + '-' +
+            leadZeros(date.getMonth() + 1) + '-' +
+            leadZeros(date.getDate()) + ' ' +
+            leadZeros(date.getHours()) + ':' +
+            leadZeros(date.getMinutes()) + ':' +
+            leadZeros(date.getSeconds());
+    }
+
     if (options.slim) {
-        return leadZeros(date.getMonth() + 1) + '/' +
+        return leadZeros(date.getMonth() + 1) + '.' +
             leadZeros(date.getDate()) + ' ' +
             leadZeros(date.getHours()) + ':' +
             leadZeros(date.getMinutes());
     }
 
-    return date.getFullYear() + '-' +
-        leadZeros(date.getMonth() + 1) + '-' +
+    return date.getFullYear() + '.' +
+        leadZeros(date.getMonth() + 1) + '.' +
         leadZeros(date.getDate()) + ' ' +
         leadZeros(date.getHours()) + ':' +
         leadZeros(date.getMinutes()) + ':' +
