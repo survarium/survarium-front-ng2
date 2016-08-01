fc8fde8c38b5a083aefea226709d197e1e70f3af
Dmitry Vasilyev <vaseker@yandex-team.ru>
2016-08-01 15:47:52 +0500

    Yandex context bundler with fallback

--- builders/direct-bundler.js
+++ builders/direct-bundler.js
@@ -10,9 +10,14 @@ let anVersion;
 let CONTEXT;
 let CONTEXT_STATIC;
 
+let target = path.join(__dirname, '..', 'app', 'services', 'direct.js');
+let fallback = path.join(__dirname, '..', 'app', 'services', 'direct_fallback.js');
+
 function write() {
     return new Promise((resolve, reject) => {
-        let target = path.join(__dirname, '..', 'app', 'services', 'direct.js');
+        if (!CONTEXT || !CONTEXT_STATIC) {
+            return reject(new Error('Content is not fully fetched'));
+        }
         
         fs.writeFile(target, `${CONTEXT} ${CONTEXT_STATIC}`, (err) => {
             if (err) {
@@ -22,6 +27,34 @@ function write() {
         });
     });
 }
+
+function copyFile(source, target) {
+    return new Promise((resolve, reject) => {
+        var cbCalled = false;
+    
+        var rd = fs.createReadStream(source);
+        rd.on('error', function(err) {
+            done(err);
+        });
+        var wr = fs.createWriteStream(target);
+        wr.on('error', function(err) {
+            done(err);
+        });
+        wr.on('close', function() {
+            done();
+        });
+        rd.pipe(wr);
+    
+        function done(err) {
+            if (!cbCalled) {
+                err ? reject(err) : resolve(target);
+                cbCalled = true;
+            }
+        }
+    });
+}
+
+console.log('Bundling Yandex direct');
 
 got('https://an.yandex.ru/system/context.js')
     .then(context => {
@@ -51,5 +84,10 @@ got('https://an.yandex.ru/system/context.js')
     })
     .then(context_static => CONTEXT_STATIC = context_static)
     .then(write)
+    .catch(err => {
+        console.error(err);
+    
+        return copyFile(fallback, target);
+    })
     .then(target => console.log(`Direct bundled to ${target}`));
         
