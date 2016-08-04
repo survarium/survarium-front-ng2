7fc789d4dea9b36528f401e523104937481dc01d
Dmitry Vasilyev <vaseker@yandex-team.ru>
2016-08-05 03:40:18 +0500

    Native context.js

--- app/async.ts
+++ app/async.ts
@@ -5,6 +5,7 @@ export function ngOnLoad(window, document) {
 
     var scripts = document.getElementsByTagName('script')[0],
         targets = [
+            { src: 'https://an.yandex.ru/system/context.js' },
             { src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js' },
             { src: 'https://vk.com/js/api/openapi.js?121' },
             { src: 'https://platform.twitter.com/widgets.js', id: 'twitter-wjs' },
