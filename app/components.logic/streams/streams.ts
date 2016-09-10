721fe3d877f96b94ff21d9d9a3d80041d494ac06
Dmitry Vasilyev <vaseker@yandex-team.ru>
2016-09-11 02:01:38 +0500

    Streams fixup

--- app/components.logic/streams/streams.ts
+++ app/components.logic/streams/streams.ts
@@ -1,16 +1,8 @@
 import { Component } from '@angular/core'
-//import { RouteConfig, AsyncRoute } from '@angular/router-deprecated'
 
 @Component({
     styles: [require('../../components.common/sub-nav/sub-nav.styl')],
     template: require('./streams.html')
 })
 
-/*@RouteConfig([
-    new AsyncRoute({ path: '/youtube', loader: () => require('es6-promise!./youtube')('Youtube'), name: 'StreamsYoutube', useAsDefault: true }),
-    new AsyncRoute({ path: '/twitch',  loader: () => require('es6-promise!./twitch')('Twitch'), name: 'StreamsTwitch' })
-])*/
-
 export class Streams {}
-
-
