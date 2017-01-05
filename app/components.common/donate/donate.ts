6478e88d1594312d39caaad1e8dd92b1100c3a4d
Dmitry Vasilyev <vaseker@yandex-team.ru>
2017-01-05 18:28:57 +0500

    Donation form revised

--- app/components.common/donate/donate.ts
+++ app/components.common/donate/donate.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core'
-import { DomSanitizer } from '@angular/platform-browser'
 
 @Component({
     selector: 'donate',
@@ -7,16 +6,6 @@ import { DomSanitizer } from '@angular/platform-browser'
     styles: [require('./donate.styl')]
 })
 
-export class Donate {
-    private src = 'https://money.yandex.ru/quickpay/shop-widget?account=41001435441981&quickpay=shop&payment-type-choice=on&mobile-payment-type-choice=on&writer=seller&targets=%D0%9D%D0%B0+%D1%80%D0%B0%D0%B7%D0%B2%D0%B8%D1%82%D0%B8%D0%B5+survarium.pro&targets-hint=&default-sum=300&button-text=03&successURL=https%3A%2F%2Fsurvarium.pro%2Finfo%2Fdonate-thanks';
-    private width = 450;
-    private height = 198;
-
-    private iframeSrc;
-
-    constructor (private _domSanitize :DomSanitizer) {
-        this.iframeSrc = this._domSanitize.bypassSecurityTrustResourceUrl(this.src);
-    }
-}
+export class Donate {}
 
 export default Donate
--- app/i18n/en.ts
+++ app/i18n/en.ts
@@ -65,11 +65,20 @@ let dict = {
     time         : 'Time',
     noData       : 'No data',
     banned       : 'Bans',
-    donate       : 'Donate',
+    donate       : {
+        title: 'Donate',
+        target: 'Donation for survarium.pro',
+        targetLabel: 'The transfer purpose',
+        comment     : 'You may write your nickname or any other information if you like.',
+        commentLabel: 'Comment',
+        amountLabel: 'Amount',
+        currency: 'RUB',
+        paymentTypeLabel: 'Payment method',
+        confirm: 'Transfer'
+    },
     donateThanks : {
         docTitle : 'Thank you for the donation',
-        thanks   : 'Thank you for the donation!',
-        info     : 'In the donation form you may write your nickname or any other information if you like. Thanks :)'
+        thanks   : 'Thank you for the donation!'
     },
     players: {
         docTitle: 'Players',
