9890e3ca3e902413760294ff6eb84e78b9643123
Dmitry Vasilyev <vaseker@yandex-team.ru>
2017-06-24 13:46:17 +0500

    More attention to AdBlock

--- app/components.common/donate/donate.ts
+++ app/components.common/donate/donate.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core'
 @Component({
     selector: 'donate',
     template: require('./donate.html'),
-    styles: [require('./donate.styl')]
+    styles: [require('./donate.styl')],
+    host: {
+        'class': 'sns'
+    }
 })
 
 export class Donate {}
