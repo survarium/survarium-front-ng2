5be1eb7925148c9b6ba1e14c1aed94a936bd41cd
Florent <florent.chavanne@econocom.com>
2021-03-19 15:19:45 +0100

    t0FF : spanish translation + uncommited files for pve and server location

---
+++ app/components.common/match-server/match-server.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'match-server',
+    inputs: ['replay'],
+    template: `{{(replay && replay.includes('node-1')) ? 'EU' 
+                    : (replay && replay.includes('node-4')) ? 'RU'
+                        : (replay && replay.includes('node-6')) ? 'NA'
+                            : (replay && replay.includes('node-21')) ? 'SEA'
+                                : (replay && replay.includes('node-19')) ? 'SA'
+                                    : '-'
+                }}`
+})
+
+export class MatchServer { }
+export default MatchServer
